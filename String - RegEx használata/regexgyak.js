8e058bcd3615fb25c277285b4184209b5683665d
Zsiga Norbert <zsiganp@gmail.com>
2024-10-09 17:16:44 +0200

    javitas10.09

--- String - RegEx használata/regexgyak.js
+++ String - RegEx használata/regexgyak.js
@@ -1,55 +1,55 @@
 let text = "Hello WORLD! Today is the 5th of June 2024 21th century. I've 15 apples, 123 oranges, and 4567 grapes."
 let regex, result
 
-Feladat1(text)
+numberLength2Or3(text)
 console.log(result)
 
-Feladat2(text)
+startsWithEven (text)
 console.log(result)
 
-Feladat3(text)
+startsWithNumber (text)
 console.log(result)
 
-Feladat4(text)
+capitalized (text)
 console.log(result)
 
-Feladat5(text)
+capitalizedLengthMin3 (text)
 console.log(result)
 
-Feladat6(text)
+startsWithVowel (text)
 console.log(result)
 
-function Feladat1(szov)
+function numberLength2Or3(szov)
 {
 regex = /\b\d{2,3}\b/g
 result = szov.match(regex)
 }
 
-function Feladat2(szov)
+function startsWithEven (szov)
 {
 regex = /\b[2468]\d*\b/g
 result = szov.match(regex)
 }
 
-function Feladat3(szov)
+function startsWithNumber (szov)
 {
 regex = /\b\d+[a-zA-Z]+\b/g
 result = szov.match(regex)
 }
 
-function Feladat4(szov)
+function capitalized (szov)
 {
 regex = /\b[A-Z][a-z]*\b/g
 result = szov.match(regex)
 }
 
-function Feladat5(szov)
+function capitalizedLengthMin3 (szov)
 {
 regex = /\b[A-Z][a-z]{2,}\b/g
 result = szov.match(regex)
 }
 
-function Feladat6(szov)
+function startsWithVowel (szov)
 {
 regex = /\b[auioe]+\b/g
 result = szov.match(regex)
